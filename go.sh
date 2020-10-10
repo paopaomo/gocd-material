975541ec94c6e824cf0bec9538e584d12ead5213
Zi Ye <zi.ye1@thoughtworks.com>
2020-10-10 15:23:32 +0800

    rename and delete unused code

--- go.sh
+++ go.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-echo "Let us pretend that this is building something ... and takes 20 seconds"
-
-count=0
-while [ "$count" -lt 20 ]; do
-    count=$((count + 1))
-
-    color_code=$((31 + (count % 7)))
-    printf "\e[01;${color_code}mBuilding [$count of 20]\e[00m\n"
-
-    sleep 1
-done
-
 cat >my-artifact.html <<EOF
 <html>
 <body>
